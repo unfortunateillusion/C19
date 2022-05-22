927118077f173aabefc2758eee309200c25bbdcc
Yuki <si9108illtakeit@gmail.com>
2022-05-22 10:23:34 +0900

    Implemented the db class

---
+++ recipe/app/src/main/java/com/example/recipe/HakamoriActivity.java
@@ -0,0 +1,29 @@
+package com.example.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class HakamoriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hakamori);
+
+        HashMap<String, String> ings = new HashMap<>();
+        ings.put("ごはん", "茶碗一杯");
+        ings.put("卵", "1個");
+        ings.put("しょうゆ", "大匙1");
+        DBHandler dbHandler = new DBHandler(HakamoriActivity.this);
+        dbHandler.addNewRecipe("卵かけごはん", ings, "ごはんをよそう。卵をかける。醤油をかける。");
+
+        RecipeModel r = dbHandler.getRecipe(1);
+        Log.d("recipe_name", r.getRecipeName());
+        Log.d("ingredients", r.getIngredients().toString());
+        Log.d("description", r.getDescription());
+    }
+}
--- recipe/app/src/main/java/com/example/recipe/MainActivity.java
+++ recipe/app/src/main/java/com/example/recipe/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callHakamori(View view) {
+        Intent intent = new Intent(this, HakamoriActivity.class);
+        startActivity(intent);
     }
 }
