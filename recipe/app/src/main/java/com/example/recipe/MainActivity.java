329dd263a97ffc89b121afaf80b2b036855ebe3a
melted-snow <69240503+melted-snow@users.noreply.github.com>
2022-05-22 13:56:30 +0900

    Revert "Kusou"

--- recipe/app/src/main/java/com/example/recipe/MainActivity.java
+++ recipe/app/src/main/java/com/example/recipe/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,25 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void Random_Output(View view) {
-        Intent intent = new Intent(getApplication(), RamdomActivity.class);
-
-        Random rand = new Random();
-        int num = rand.nextInt(10);
-        intent.putExtra("choice", num + 1);
-
-        intent.putExtra("Where_from", "main");
-
-        //行く
-        startActivity(intent);
-    }
-
-    public void list_Output(View view) {
-        Intent intent = new Intent(getApplication(), ListActivity.class);
-
-        //行く
-        startActivity(intent);
     }
 }
